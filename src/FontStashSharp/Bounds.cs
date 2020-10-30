ea0ff21cbb6f989db5424d082d7ede48d6fb0b45
James <james@epicguru.co.uk>
2020-10-30 14:06:52 +0000

    Added ToString to Bounds.

--- src/FontStashSharp/Bounds.cs
+++ src/FontStashSharp/Bounds.cs
@@ -3,5 +3,10 @@
 	public struct Bounds
 	{
 		public float X, Y, X2, Y2;
-	}
+
+        public override string ToString()
+        {
+            return $"[x: {X}, y: {Y}, x2: {X2}, y2: {Y2}]";
+        }
+    }
 }
